ba9b4b31606bd5b3c8b21a7ebc6a53308e061b63
Lukong27 <dannylu1999@gmail.com>
2021-09-27 15:20:02 +0200

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("How are you?");     // Test comment
+
+    System.out.println("");
+
+    System.out.print("Goodbye, ");          // Test comment 2
+    System.out.println("cruel world");
+
+    System.out.println("");
+
+    System.out.print("Hello! \nHow are you doing?\nTest\n");
+
+    System.out.println("");
+
+    System.out.println("She said \"Hello!\" to me.");
+    System.out.println("She said \tHello!\t to me.");
+
+  }
+}
